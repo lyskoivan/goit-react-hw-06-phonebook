a9c56ea07e7b9bb33dda089a99f8e9b48c3c8d02
lyskoivan <lysko1999i@gmail.com>
2020-02-18 03:11:23 +0200

    added redux toolkit

--- src/redux/phonebook/phonebookActions.js
+++ src/redux/phonebook/phonebookActions.js
@@ -1,21 +1,39 @@
+import { createAction } from '@reduxjs/toolkit';
+
 import * as types from './phonebookTypes';
 
-export const addContact = contact => ({
-  type: types.ADD_CONTACT,
+export const addContact = createAction(types.ADD_CONTACT, contact => ({
   payload: { contact },
-});
+}));
 
-export const takeContacts = contacts => ({
-  type: types.TAKE_CONTACTS,
+export const takeContacts = createAction(types.TAKE_CONTACTS, contacts => ({
   payload: { contacts },
-});
+}));
 
-export const deleteContact = id => ({
-  type: types.DELETE_CONTACT,
+export const deleteContact = createAction(types.DELETE_CONTACT, id => ({
   payload: { id },
-});
+}));
 
-export const filterUpdate = value => ({
-  type: types.FILTER_UPDATE,
+export const filterUpdate = createAction(types.FILTER_UPDATE, value => ({
   payload: { value },
-});
+}));
+
+// export const addContact = contact => ({
+//   type: types.ADD_CONTACT,
+//   payload: { contact },
+// });
+
+// export const takeContacts = contacts => ({
+//   type: types.TAKE_CONTACTS,
+//   payload: { contacts },
+// });
+
+// export const deleteContact = id => ({
+//   type: types.DELETE_CONTACT,
+//   payload: { id },
+// });
+
+// export const filterUpdate = value => ({
+//   type: types.FILTER_UPDATE,
+//   payload: { value },
+// });
--- src/redux/phonebook/phonebookReducers.js
+++ src/redux/phonebook/phonebookReducers.js
@@ -1,32 +1,45 @@
 import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 
-import * as types from './phonebookTypes';
+// import * as types from './phonebookTypes';
+import * as Actions from './phonebookActions';
 
-const contactsReducer = (state = [], action) => {
-  switch (action.type) {
-    case types.ADD_CONTACT:
-      return [action.payload.contact, ...state];
+const contactsReducer = createReducer([], {
+  [Actions.addContact]: (state, action) => [action.payload.contact, ...state],
+  [Actions.takeContacts]: (state, action) => action.payload.contacts,
+  [Actions.deleteContact]: (state, action) =>
+    state.filter(contact => contact.id !== action.payload.id),
+});
 
-    case types.TAKE_CONTACTS:
-      return action.payload.contacts;
+const filterReducer = createReducer('', {
+  [Actions.filterUpdate]: (state, action) => action.payload.value,
+});
 
-    case types.DELETE_CONTACT:
-      return state.filter(contact => contact.id !== action.payload.id);
+// const contactsReducer = (state = [], action) => {
+//   switch (action.type) {
+//     case types.ADD_CONTACT:
+//       return [action.payload.contact, ...state];
 
-    default:
-      return state;
-  }
-};
+//     case types.TAKE_CONTACTS:
+//       return action.payload.contacts;
 
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case types.FILTER_UPDATE:
-      return action.payload.value;
+//     case types.DELETE_CONTACT:
+//       return state.filter(contact => contact.id !== action.payload.id);
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
+
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case types.FILTER_UPDATE:
+//       return action.payload.value;
+
+//     default:
+//       return state;
+//   }
+// };
 
 const reducer = combineReducers({
   contacts: contactsReducer,
