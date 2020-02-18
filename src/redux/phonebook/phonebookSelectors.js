82a86181c5fc2efa988a59e81ee4fea0db684a18
lyskoivan <lysko1999i@gmail.com>
2020-02-18 02:38:30 +0200

    added reselect lib

--- src/redux/phonebook/phonebookSelectors.js
+++ src/redux/phonebook/phonebookSelectors.js
@@ -1,9 +1,13 @@
+import { createSelector } from 'reselect';
+
 export const getContacts = store => store.phonebook.contacts;
 
 export const getFilter = store => store.phonebook.filter;
 
-export const getFiltredContacts = store => {
-  return getContacts(store).filter(contact =>
-    contact.name.toLowerCase().includes(getFilter(store).toLowerCase()),
-  );
-};
+export const getFiltredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filtred) =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filtred.toLowerCase()),
+    ),
+);
